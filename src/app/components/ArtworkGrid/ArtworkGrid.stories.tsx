f13c3d66b830d21846ab26a7ae65c54e116972c3
Jen Udan <judan@8thlight.com>
2025-04-03 15:30:03 -0500

    Update component story template and ArtworkGrid stories file

--- src/app/components/ArtworkGrid/ArtworkGrid.stories.tsx
+++ src/app/components/ArtworkGrid/ArtworkGrid.stories.tsx
@@ -1,20 +1,70 @@
-import { Story } from '@storybook/react/types-6-0';
+import type { Meta, StoryObj } from '@storybook/react';
 import ArtworkGrid, { ArtworkGridProps } from './ArtworkGrid';
 
-export default {
+const meta: Meta<typeof ArtworkGrid> = {
   component: ArtworkGrid,
-  title: 'Components/ArtworkGrid',
-  parameters: {
-    componentSubtitle: 'Short description of component.',
-  },
-}
+};
 
-const Template: Story<ArtworkGridProps> = (args) => (
-  <ArtworkGrid {...args}>
-    <div>Hello world.</div>
-  </ArtworkGrid>
-);
+export default meta;
+type Story = StoryObj<typeof ArtworkGrid>;
 
-export const Primary = Template.bind({});
-
-Primary.args = {};
+export const Primary: Story = {
+  args: {
+    artworks: [
+      {
+        id: '1',
+        title: 'Mona Lisa',
+        thumbnail: {
+          lqip: 'https://placehold.co/400',
+          width: 400,
+          height: 400,
+        },
+      },
+      {
+        id: '2',
+        title: 'Starry Night',
+        thumbnail: {
+          lqip: 'https://placehold.co/400',
+          width: 400,
+          height: 400,
+        },
+      },
+      {
+        id: '3',
+        title: 'The Kiss',
+        thumbnail: {
+          lqip: 'https://placehold.co/400',
+          width: 400,
+          height: 400,
+        },
+      },
+      {
+        id: '4',
+        title: 'Girl with a Pearl Earring',
+        thumbnail: {
+          lqip: 'https://placehold.co/400',
+          width: 400,
+          height: 400,
+        },
+      },
+      {
+        id: '5',
+        title: 'American Gothic',
+        thumbnail: {
+          lqip: 'https://placehold.co/400',
+          width: 400,
+          height: 400,
+        },
+      },
+      {
+        id: '6',
+        title: 'The Birth of Venus',
+        thumbnail: {
+          lqip: 'https://placehold.co/400',
+          width: 400,
+          height: 400,
+        },
+      },
+    ],
+  } satisfies ArtworkGridProps,
+};
