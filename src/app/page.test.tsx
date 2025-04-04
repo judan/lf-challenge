6ca5002b4162aea205ba363bef71d8a50f34ff77
Jen Udan <judan@8thlight.com>
2025-04-04 09:10:31 -0500

    Add a page.test.tsx file to test API fetch call

---
+++ src/app/page.test.tsx
@@ -0,0 +1,27 @@
+import { beforeAll, describe, expect, expectTypeOf, test } from 'vitest';
+
+const timeout = 30000;
+
+describe('Request Earth Polychromatic Imaging Camera', () => {
+  let response: Response;
+  let body: Array<{ [key: string]: unknown }>;
+
+  beforeAll(async () => {
+    response = await fetch(
+      'https://api.artic.edu/api/v1/artworks?page=1&limit=100',
+    );
+    body = await response.json();
+  }, timeout);
+
+  test('Should have response status 200', () => {
+    expect(response.status).toBe(200);
+  });
+
+  test('Should have content-type', () => {
+    expect(response.headers.get('Content-Type')).toBe('application/json');
+  });
+
+  test('Should have array in the body', () => {
+    expectTypeOf(body).toBeArray();
+  });
+});
